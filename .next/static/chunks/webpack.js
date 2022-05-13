5adfc72e8770ae23ae6e87dbfb140c0ab249265c
ocastrojoao <contatoocastrojoao@gmail.com>
2022-05-13 01:50:44 -0300

    arquivo article e framer motion

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "85fa9856cb8bcbfe"; }
+/******/ 		__webpack_require__.h = function() { return "d98dee1c7fe6f413"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from  '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
+import { AnimatePresence } from 'framer-motion'
 
 const Website = ( { Component, pageProps, router } ) => {
     return (
         <ChakraProvider theme={theme}>
             <Fonts />
             <Layout router={router}>
+                <AnimatePresence exitBeforeEnter initial={true}>
+
                 <Component {...pageProps} Key={router.route} />
-            </Layout>
+                </AnimatePresence>
+                </Layout>
         </ChakraProvider>
     )
 }
