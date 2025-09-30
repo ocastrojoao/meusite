40f0c034888e29bfcf793355adca5953ced0d6ff
UCOB - APLAC - João Henrique <joao.henrique@adventistas.org>
2025-09-30 11:31:53 -0300

    ajustes

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,17 +1,19 @@
+// pages/_document.js (corrigido)
+
 import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, { Html, Head, Main, NextScript} from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
 
-export default class Document extends NextDocument{
-    render ( ) {
-        return(
+export default class Document extends NextDocument {
+    render() {
+        return (
             <Html lang="en">
                 <Head />
-                <Body>
+                <body>  {/* <-- A CORREÇÃO ESTÁ AQUI */}
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                <Main />
-                <NextScript />
-                </Body>
+                    <Main />
+                    <NextScript />
+                </body> {/* <-- E AQUI */}
             </Html>
         )
     }
